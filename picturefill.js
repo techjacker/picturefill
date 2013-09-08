16f0a7b5dade519f64d2269676a56a4e7120d080
Andy <mail@andrewgriffithsonline.com>
2013-09-08 16:32:04 +0100

    reload matchmedia every time

--- picturefill.js
+++ picturefill.js
@@ -11,6 +11,7 @@ module.exports = function (w) {
 	"use strict";
 
 	w.picturefill = function() {
+		addMatchMediaSupport();
 		var ps = w.document.getElementsByTagName( "span" );
 
 		// Loop the pictures
@@ -63,7 +64,7 @@ module.exports = function (w) {
 		w.attachEvent( "onload", w.picturefill );
 	}
 
-	return picturefill;
+	return w.picturefill;
 
 };
 // }( this ));
